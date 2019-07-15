9c2e8b349a0091e84b4a2558297c41ea522719e5
gandol2 <sskim2333@gmail.com>
2019-07-15 17:57:40 +0900

    Update 01_adflow_cron_add.sh

--- 01_adflow_cron_add.sh
+++ 01_adflow_cron_add.sh
@@ -3,12 +3,12 @@ echo "===================[cron 작업][START]"
 echo "[INFO] cron 삭제중.."
 crontab -r
 echo "[INFO] cron 작업 등록중.."
-cat <(crontab -l) <(echo "* * * * * sleep 0 & wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
-cat <(crontab -l) <(echo "* * * * * sleep 10 & wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
-cat <(crontab -l) <(echo "* * * * * sleep 20 & wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
-cat <(crontab -l) <(echo "* * * * * sleep 30 & wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
-cat <(crontab -l) <(echo "* * * * * sleep 40 & wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
-cat <(crontab -l) <(echo "* * * * * sleep 50 & wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
+cat <(crontab -l) <(echo "* * * * * sleep 0 && wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
+cat <(crontab -l) <(echo "* * * * * sleep 10 && wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
+cat <(crontab -l) <(echo "* * * * * sleep 20 && wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
+cat <(crontab -l) <(echo "* * * * * sleep 30 && wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
+cat <(crontab -l) <(echo "* * * * * sleep 40 && wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
+cat <(crontab -l) <(echo "* * * * * sleep 50 && wget -q -O - http://127.0.0.1/wp-cron.php;") | crontab -
 cat <(crontab -l) <(echo "* * * * * sh -x /root/adflow_wp_config/mysqlfix.sh;") | crontab -
 crontab -l
 echo "[INFO] cron 서비스 재시작중.."
